5689bf432df456745a6a646b6d56100cd0a1c2e6
John Thomas <jthomas387@liberty.edu>
2021-03-31 13:56:10 -0400

    Add project files.

---
+++ ThomasVectors/ThomasVectors.cpp
@@ -0,0 +1,26 @@
+// John Thomas
+// C++ Vectors
+//
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> g1;
+
+	for (int i = 1; i <= 5; i++)
+		g1.push_back(i);
+	g1.insert(g1.begin(), 61);
+	cout << "Numbers in vector: ";
+	for (int i = 0; i < g1.size(); i++)
+		cout << g1[i] << " ";
+
+	cout << "\n\nNumbers in reverse: ";;
+	for (int i = g1.size() - 1; i >= 0; i--)
+		cout << g1[i] << " ";
+
+	cout << "\n\nEnd of Program\n\n";
+	system("pause");;
+	return 0;
+}
